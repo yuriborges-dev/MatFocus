374a47546876bc251c5e1eeac6e522d392a260c9
yuuri8 <yuriborges1011@gmail.com>
2026-03-21 22:19:54 -0300

    feat(router): configurar navegação inicial com React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,7 @@
+import AppRoutes from "./routes"
+
 function App() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">
-        MatFocus funcionando 🚀
-      </h1>
-    </div>
-  )
+  return <AppRoutes />
 }
 
 export default App
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+function LoginPage() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-100">
+      <h1 className="text-3xl font-bold">Tela de Login</h1>
+    </div>
+  )
+}
+
+export default LoginPage
